83e8397c75fa6333b591555ff79e4277a924ff79
iamanujup <iamanujup79@gmail.com>
2026-04-06 16:20:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -92,6 +92,20 @@ ongoing_edits   = {}
 user_quiz_data  = {}
 broadcast_active = False
 TEMP_ACCESS     = {}
+PORT = int(os.environ.get("PORT", 10000))
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_server():
+    server = HTTPServer(("0.0.0.0", PORT), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_server, daemon=True).start()
+
 
 import binascii
 MASTER_KEY_HEX = binascii.hexlify(MASTER_KEY.encode() if isinstance(MASTER_KEY, str) else MASTER_KEY).decode()
